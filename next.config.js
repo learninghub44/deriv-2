817839ddfe5bf3b6d3d36cfadebe7bef6feca61f
learninghub44 <chris@learninghub44.dev>
2026-07-10 04:42:06 +0000

    Add output: export for Cloudflare Pages static hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@deriv/core'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
